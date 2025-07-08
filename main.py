13eeb0d463bc6e7a021f128b46fb51a48d77f6f1
Angel Nicole Viernes Iniego <angel.v.iniego357@gmail.com>
2025-07-08 10:16:54 +0800

    Added initial nodes from yesterday's data gathering

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ for u, v, data in G.edges(data=True):
     print(f"{u} — {v} : {data['weight']}s, {data['label']}")
 
 # Example: find shortest path based on time
-shortest_path = nx.shortest_path(G, "UC", "Wean", weight='weight')
-total_time = nx.shortest_path_length(G, "UC", "Wean", weight='weight')
+shortest_path = nx.shortest_path(G, "CS Admin Building", "Area 2", weight='weight')
+total_time = nx.shortest_path_length(G, "CS Admin Building", "Area 2", weight='weight')
 
-print("\nShortest path from UC to Wean:")
+print("\nShortest path from CS Admin Building to Area 2:")
 print(" → ".join(shortest_path))
 print(f"Total time: {total_time} seconds")
